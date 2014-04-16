4fe8ffe2fd56eae8b2d9a0373b1839b32d8b22dd
Neo Pragma LLC <neopragma@ubuntu.(none)>
2014-04-16 13:41:11 -0700

    Added evil code

--- src/main/java/atm/client/Atm.java
+++ src/main/java/atm/client/Atm.java
@@ -3,19 +3,45 @@ package atm.client;
 import backend.call.wrapper.BackendCallException;
 import backend.call.wrapper.BackendCallWrapper;
 import backend.call.wrapper.BackendCallWrapperImpl;
+import backend.call.wrapper.BackendCallbackListener;
 import backend.call.wrapper.BackendFunction;
 import backend.call.wrapper.BackendRequest;
 import backend.call.wrapper.BackendResponse;
 
-public class Atm {
-
+@SuppressWarnings("unused")
+public class Atm implements BackendCallbackListener {
+	
+	private static final BackendCallWrapper be = new BackendCallWrapperImpl();
+	private BackendResponse res;
+	private BackendCallbackListener listener;
+	
+	/**
+	 * Allow injection of alternative callback listener.
+	 */
+	public void setBackendCallbackListener(BackendCallbackListener listener) {
+		listener = listener;
+	}
+	
+	/**
+	 * Get balance
+	 * 
+	 * @param accountNumber
+	 * @return balance
+	 * @throws BackendCallException
+	 */
 	public Double getBalance(String accountNumber) throws BackendCallException {
 		BackendRequest req = new BackendRequest();
 		req.setAccountNumber1(accountNumber);
 		BackendCallWrapper be = new BackendCallWrapperImpl();
+		// Call back end asynchronously
 		BackendResponse res = be.callAndWait(BackendFunction.ACCOUNT, req, 0);
 		return res.getBalance();
 	}
-	
+
+	@Override
+	public void backendCallback(BackendResponse response) {
+		this.res = response;
+		
+	}
 	
 }
